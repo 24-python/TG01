e337b85bb0d05cda057de48278acc01e7ca04590
24-python <24_python@mail.ru>
2025-03-15 09:41:51 +0300

    1

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ import random
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# @dp.message(Command('photo', prefix='!')) # префикс перед командой
+# async def photo(message: Message):
+#     list = ['https://saltmag.ru/media/articles/inner/2020/6373/2_gIzSf7E.jpg',
+#             'https://saltmag.ru/media/articles/inner/2020/6373/1_goqINJP.jpg',
+#             'https://saltmag.ru/media/articles/inner/2020/6373/3_cRVfFCG.jpg'
+#             ]
+#     rand_photo = random.choice(list)
+#     await message.answer_photo(photo=rand_photo, caption='Супер фотка')
+
 @dp.message(Command('photo'))
 async def photo(message: Message):
     list = ['https://saltmag.ru/media/articles/inner/2020/6373/2_gIzSf7E.jpg',
@@ -31,9 +40,18 @@ async def aitext(message: Message):
 async def help(message: Message):
     await message.answer('Этот бот умеет выполнять команды: \n /start - запуск бота \n /help - помощь')
 
+# @dp.message(CommandStart()) # простое привестсвие
+# async def start(message: Message):
+#     await message.answer('Hi! Я - БОТ')
+
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer('Hi! Я - БОТ')
+    await message.answer(f'Hi, {message.from_user.first_name}') # привестсвие с именем
+
+@dp.message()
+async def start(message: Message):
+    await message.send_copy(chat_id=message.chat.id)
+
 
 async def main():
     await dp.start_polling(bot)
